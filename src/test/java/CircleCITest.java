c0a0df2e1355b14fc2aaef113360deade8ea82dd
Henry Darana <henry.darana@tray.com>
2023-03-27 21:18:01 +0530

    validateBerryFirmnessResponseTimeUnder5Seconds

--- src/test/java/CircleCITest.java
+++ src/test/java/CircleCITest.java
@@ -9,8 +9,16 @@ import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
 public class CircleCITest {
 
     @Test
-    public void responseTimeUnder5Seconds() throws IOException {
+    public void validateBerryResponseTimeUnder5Seconds() throws IOException {
         TestPlanStats stats = testPlan(threadGroup(1, 2, httpSampler(Routes.getBerryEndpoint(1)))).run();
+        Assert.assertTrue(stats.overall().sampleTimePercentile99().getSeconds() <= 5);
+    }
+
+    @Test
+    public void validateBerryFirmnessResponseTimeUnder5Seconds() throws IOException {
+        TestPlanStats stats = testPlan(threadGroup(1, 1,
+                httpSampler(Routes.getBerryEndpoint(1)).children(jsonExtractor("FIRMNESS_PATH", "firmness.url")),
+                httpSampler("${FIRMNESS_PATH}"))).run();
         Assert.assertTrue(stats.overall().sampleTimePercentile99().getSeconds() <= 5);
     }
 }
